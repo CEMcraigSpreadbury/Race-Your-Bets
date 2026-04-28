e0121cf55ce11821964be82035a3a8ac7771860e
CraigSpreadbury <craigs1995@gmail.com>
2026-04-28 10:58:16 +0100

    sounds fix

--- client/src/sounds.js
+++ client/src/sounds.js
@@ -18,24 +18,10 @@ const RANDOM_SOUNDS = [
   'Will',
 ].map((name) => `/sounds/random/${name}.aac`);
 
-const cache = {};
-
-function load(src) {
-  if (!cache[src]) {
-    const a = new Audio(src);
-    a.preload = 'auto';
-    cache[src] = a;
-  }
-  return cache[src];
-}
-
 function play(src) {
-  const a = load(src);
-  a.currentTime = 0;
+  const a = new Audio(src);
   a.play().catch(() => {});
 }
-
-['/sounds/race start.aac', '/sounds/race finish.aac', ...RANDOM_SOUNDS].forEach(load);
 
 export function playRaceStart()  { play('/sounds/race start.aac'); }
 export function playRaceFinish() { play('/sounds/race finish.aac'); }
